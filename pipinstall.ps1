ff5c7c84f8024d4fa9a27c67751195d67c846a42
Mario Jauvin <marioja@users.noreply.github.com>
2025-03-16 21:54:30 -0400

    Add pip install that works (still hangs but more testing)

---
+++ pipinstall.ps1
@@ -0,0 +1,5 @@
+pip install torch torchaudio torchvision --index-url https://download.pytorch.org/whl/cu124
+pip install packaging
+pip install wheel
+pip install flash_attn
+pip install -r requirements.txt
