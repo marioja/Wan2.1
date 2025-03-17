364ed8fde8369ec873f1104bfeb866259e798020
Mario Jauvin <marioja@users.noreply.github.com>
2025-03-16 22:22:07 -0400

    Upgrade pip and comments

--- pipinstall.ps1
+++ pipinstall.ps1
@@ -1,3 +1,6 @@
+# Note:
+# - Python 3.12 required for this to work.
+python -m pip install --upgrade pip
 pip install torch torchaudio torchvision --index-url https://download.pytorch.org/whl/cu124
 pip install packaging
 pip install wheel
